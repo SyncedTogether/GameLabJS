14d2728a8e060c56fef8e099989965ef975ce3c7
SyncedTogether <xdskyward@gmail.com>
2023-04-14 00:40:49 -0700

    canvas

---
+++ js/const.js
@@ -0,0 +1,9 @@
+const editModes = {
+  Select: 0,
+  Pan: 1,
+  Zoom: 2,
+  Scale: 3,
+  Rotate: 4,
+};
+
+const defaultEditMode = editModes.Select;
--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,3 @@
 const filePathDisplay = document.getElementById("filepath-display");
+const canvas = document.getElementById("preview-canvas");
+const ctx = canvas.getContext("2d");
